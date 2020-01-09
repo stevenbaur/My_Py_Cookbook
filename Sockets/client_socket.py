72fb1f35d81115a2f80a9111ded81c3456766c55
Steven Baur <baur.steven@googlemail.com>
2020-01-09 18:49:56 +0100

    added comments

--- Sockets/client_socket.py
+++ Sockets/client_socket.py
@@ -1,7 +1,7 @@
-import socket
-client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+import socket                                                       #importiere sockets
+client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)   #definiere einen socket AF_INET = ipV4, SOCK_STREAM = TCP
 
-server_addr = ('127.0.0.1', 1337)
-client_socket.connect(server_addr)
-msg = "Message to send as a string"
-client_socket.send(bytes(msg,'utf8'))
+server_addr = ('127.0.0.1', 1337)                                   #Adresse des Servers als Tupel (IP,PORT)
+client_socket.connect(server_addr)                                  #Verbinde zum Server
+msg = "Message to send as a string"                                 #Zu übertragende Nachricht
+client_socket.send(bytes(msg,'utf8'))                               #Nachricht wird übertragen (es werden immer bytes übertragen!)
--- Sockets/server_socket.py
+++ Sockets/server_socket.py
@@ -1,10 +1,13 @@
-import socket
+import socket                                                       #importiere sockets
 
-server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server_socket.bind(('127.0.0.1', 1337))
-server_socket.listen(1)
+server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)   #definiere einen socket AF_INET = ipV4, SOCK_STREAM = TCP
+server_socket.bind(('127.0.0.1', 1337))                             #Auf welche Adresse soll der Server hören? (bei Internet-server empfiehlt sich gethostname() )
+server_socket.listen(1)                                             #starte das Hören für n clients (bei mehr als 1 unbedingt Threading nutzen!)
 
-while True:
-    (client_socket, addr) = server_socket.accept()
-    msg = str(client_socket.recv(512),'utf8')
-    print(msg)
+while True:                                                         #Dauerschleife, in der der Server läuft
+    (client_socket, addr) = server_socket.accept()                  #Verbindung vom Client akzeptieren
+    msg = str(client_socket.recv(512),'utf8')                       #Empfange die Nachricht und konvertiere Bytes zurück in String
+                                                                    #recv(Buffergröße)
+    print(msg)                                                      #Zeige die Nachricht an.
+                                                                    #Bei komplexeren Themen empfiehlt es sich, dass der jeweilige client(thread) eine Klasse erzeugt,
+                                                                    #die wiederum die gewünschten Aufgaben erfüllt
